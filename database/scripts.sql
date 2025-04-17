aedeec69c71768040814e18c285ba9163c7a8b5c
KAIKE BUENO <24122303@CFP127ALUNO.LOCAL>
2025-04-17 15:34:01 -0300

    Atualizando scripts SQL

--- database/scripts.sql
+++ database/scripts.sql
@@ -15,7 +15,37 @@ create table tbl_musica (
   letra 			text
 );
 
-#testando as tabelas
+create table tbl_genero (
+  id	 			int not null auto_increment primary key,
+  nome	 			varchar (100) 		 not null
+);
+
+create table tbl_usuario (
+  id	 			int not null auto_increment primary key,
+  nome	 			varchar (100) 		 not null,
+  email	 			varchar (200) 		 not null,
+  senha	 			varchar (200) 		 not null,
+  foto_perfil  		varchar(300) 
+	
+);
+
+create table tbl_artista (
+  id	 			int not null auto_increment primary key,
+  nome             	varchar (100)	 not null,
+  nome_artistico    varchar (100)	 not null,
+  biografia 		text 	 		 not null,
+  nacionalidade     varchar (100)	 not null,
+  foto_perfil  		varchar(300) 
+	
+);
+
+
 show tables;
-desc tbl_musica;
-select * from tbl_musica;
+
+desc tbl_musica,
+desc tbl_genero
+select * from tbl_genero
+
+SELECT * FROM tbl_musica WHERE id = 2;
+SELECT * FROM tbl_genero WHERE id = 1;
+
